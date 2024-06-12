c31037a8157274d976656d2c1d6f740d98ff4550
MuazZulkarnain <idmuazzulkarnain@gmail.com>
2024-06-12 16:47:29 +0800

    done some housekeeping to output clear comment and added some space in between output

--- startDev.sh
+++ startDev.sh
@@ -2,6 +2,8 @@
 # docker rmi $(docker images --quiet --filter "dangling=true")
 # docker volume rm $(docker volume ls -qf dangling=true)
 
+echo
+echo
 echo '########## Cleaning everything (docker images, volumes, everything...) ##########'
 sleep 2s
 docker rm -vf $(docker ps -a -q) 
@@ -9,6 +11,8 @@ docker system prune --all
 docker volume prune --filter all=1
 sleep 2s
 
+echo
+echo
 echo '########## creating ca materials for Laptop 1 ##########'
 sleep 2s
 cd ./setup1/Laptop1
@@ -22,6 +26,8 @@ sleep 1s
 ./create-certificate-with-ca.sh
 cd ../../
 
+echo
+echo
 echo '########## creating ca materials for Laptop 2 ##########'
 sleep 2s
 cd ./Laptop2
@@ -35,6 +41,8 @@ sleep 1s
 ./create-certificate-with-ca.sh
 cd ../../
 
+echo
+echo
 echo '########## creating ca materials for Pi 4 ##########'
 sleep 2s
 cd ./Pi4
@@ -48,6 +56,8 @@ sleep 1s
 ./create-certificate-with-ca.sh
 cd ../../
 
+echo 
+echo
 echo '########## creating ca materials for Pi 5 ##########'
 sleep 2s
 cd ./Pi5
@@ -61,8 +71,10 @@ sleep 1s
 ./create-certificate-with-ca.sh
 cd ../../
 
-# echo '########## show all running containers ##########'
-# docker ps
+echo
+echo
+echo '########## show all running containers ##########'
+docker ps
 
 # echo '########## bootstrap the blockchain, creating genesis block and anchor peers ##########'
 # cd ../artifacts/channel
